ecb8424e5c55d5285e3fead5b6e3c1d0ad9caf4e
Lan Ejiri <lan.ejiri@yahoo.com>
2018-05-01 13:41:42 -0400

    oops

--- controllers/stuff_controllers.js
+++ controllers/stuff_controllers.js
@@ -6,12 +6,8 @@ var router = express.Router();
 var db = require("../models");
 var Sequelize = require("sequelize");
 
-<<<<<<< HEAD
-router.get("/signUp", function (req, res) {
-=======
 //sign up page
 router.get("/signUp", function(req, res) {
->>>>>>> 48d5b83d11a859e242f90c66e5b1a069c89c2279
   // need to wrap the binary image
   db.Category.findAll({})
     .then(function (result) {
@@ -33,24 +29,13 @@ router.get("/signUp", function(req, res) {
         }
         anotherObject.Item = results;
         anotherObject.user = req.user ? req.user.id : null;
-<<<<<<< HEAD
-
-        // console.log("ANOTHER OBJECT: ", anotherObject);
-        // console.log("USER: ");
-
-=======
->>>>>>> 48d5b83d11a859e242f90c66e5b1a069c89c2279
         res.render("signUp", anotherObject);
       })
     );
 });
 
-<<<<<<< HEAD
-router.get("/login", function (req, res) {
-=======
 //login page
 router.get("/login", function(req, res) {
->>>>>>> 48d5b83d11a859e242f90c66e5b1a069c89c2279
   // need to wrap the binary image
   db.Category.findAll({})
     .then(function (result) {
@@ -71,13 +56,6 @@ router.get("/login", function(req, res) {
         }
         anotherObject.Item = results;
         anotherObject.user = req.user ? req.user.id : null;
-<<<<<<< HEAD
-
-        // console.log("ANOTHER OBJECT: ", anotherObject);
-        // console.log("USER: ");
-
-=======
->>>>>>> 48d5b83d11a859e242f90c66e5b1a069c89c2279
         res.render("logIn", anotherObject);
       })
     );
@@ -128,14 +106,9 @@ router.get("/newlisting", function (req, res) {
     );
 });
 
-<<<<<<< HEAD
-router.post("/newlisting", function (req, res) {
-  db.Item.create(req.body).then(function (result) {
-=======
 //post the new listing
 router.post("/newlisting", function(req, res) {
   db.Item.create(req.body).then(function(result) {
->>>>>>> 48d5b83d11a859e242f90c66e5b1a069c89c2279
     res.redirect("/");
   });
 });
@@ -242,7 +215,6 @@ router.get("/iteminfo1/:id", function (req, res) {
   // });
 });
 
-<<<<<<< HEAD
 
 
 //manager
@@ -356,9 +328,6 @@ router.get("/iteminfo1/:id", function (req, res) {
   });
 });
 
-=======
-//sort by category
->>>>>>> 48d5b83d11a859e242f90c66e5b1a069c89c2279
 var catObj = {};
 var catNumber;
 router.get("/category/:category", function (req, res) {
@@ -388,14 +357,9 @@ router.get("/category/:category", function (req, res) {
     });
 });
 
-<<<<<<< HEAD
-router.get("/about", function (req, res) {
-  db.Category.findAll({}).then(function (result) {
-=======
 //about page
 router.get("/about", function(req, res) {
   db.Category.findAll({}).then(function(result) {
->>>>>>> 48d5b83d11a859e242f90c66e5b1a069c89c2279
     anotherObject = {
       categories: result
     };
@@ -403,14 +367,9 @@ router.get("/about", function(req, res) {
   });
 });
 
-<<<<<<< HEAD
-router.get("/howitworks", function (req, res) {
-  db.Category.findAll({}).then(function (result) {
-=======
 //howitworks page
 router.get("/howitworks", function(req, res) {
   db.Category.findAll({}).then(function(result) {
->>>>>>> 48d5b83d11a859e242f90c66e5b1a069c89c2279
     anotherObject = {
       categories: result
     };
